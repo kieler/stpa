64b059377ec67a17b8cc9ec5165737676de6f02d
Jette Petzold <stu201975@mail.uni-kiel.de>
2021-11-23 14:10:43 +0100

    basic grammar

--- extension/src/language-server/stpa-scopeProvider.ts
+++ extension/src/language-server/stpa-scopeProvider.ts
@@ -1,5 +1,5 @@
-import { AstNode, AstNodeDescription, DefaultScopeProvider, getDocument, Scope, SimpleScope, Stream, stream } from "langium";
-import {  isResps, isSystemUCAs, Model, Node} from "./generated/ast";
+import { AstNode, AstNodeDescription, DefaultScopeProvider, getDocument, PrecomputedScopes, Scope, SimpleScope, Stream, stream } from "langium";
+import { isResps, isSystemUCAs, Model, Node, UCA} from "./generated/ast";
 import { StpaServices } from "./stpa-module";
 
 
@@ -15,11 +15,40 @@ export class STPAScopeProvider extends DefaultScopeProvider {
 
         const precomputed = getDocument(node).precomputedScopes;
         if (precomputed) {
-            let currentNode: AstNode | undefined = node;
-            if ((isResps(node) || isSystemUCAs(node)) && referenceType == Node) {
-                const model = node.$container as Model
-                currentNode = model.controlStructure
+            if (referenceType == UCA) {
+                return this.getUCAs(node, referenceType, precomputed)
+            } else {
+                let currentNode: AstNode | undefined = node;
+                // responsibilities and UCAs should have references to the nodes in the control structure
+                if ((isResps(node) || isSystemUCAs(node)) && referenceType == Node) {
+                    const model = node.$container as Model
+                    currentNode = model.controlStructure
+                } 
+
+                do {
+                    const allDescriptions = precomputed.get(currentNode);
+                    if (allDescriptions) {
+                        scopes.push(stream(allDescriptions).filter(
+                            desc => this.reflection.isSubtype(desc.type, referenceType)));
+                    }
+                    currentNode = currentNode.$container;
+                } while (currentNode);
             }
+        }
+
+        let result: Scope = this.getGlobalScope(referenceType);
+        for (let i = scopes.length - 1; i >= 0; i--) {
+            result = new SimpleScope(scopes[i], result);
+        }
+        return result;
+    }
+    
+    private getUCAs(node: AstNode, referenceType: string, precomputed: PrecomputedScopes): Scope {
+        const model = node.$container as Model
+        const scopes: Array<Stream<AstNodeDescription>> = [];
+        const allUCAs = model.allUCAs
+        for (const systemUCAs of allUCAs) {
+            let currentNode: AstNode | undefined = systemUCAs;
             do {
                 const allDescriptions = precomputed.get(currentNode);
                 if (allDescriptions) {
@@ -35,7 +64,7 @@ export class STPAScopeProvider extends DefaultScopeProvider {
             result = new SimpleScope(scopes[i], result);
         }
         return result;
-    }
 
+    }
 
 }
