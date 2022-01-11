e4bd2a7d4fea2e6508383377ef9ab8280f02c5cf
Jette Petzold <stu201975@mail.uni-kiel.de>
2022-01-11 16:13:23 +0100

    forgotten file

--- language-server/src/layout-config.ts
+++ language-server/src/layout-config.ts
@@ -1,6 +1,10 @@
 import { LayoutOptions } from 'elkjs';
 import { DefaultLayoutConfigurator } from 'sprotty-elk/lib/elk-layout';
-import { SGraph, SModelIndex, SNode, SPort } from 'sprotty-protocol';
+import { getBasicType, SEdge, SGraph, SLabel, SModelElement, SModelIndex, SNode, SPort, SShapeElement } from 'sprotty-protocol';
+import { STPANode } from './STPA-interfaces';
+import { CS_NODE_TYPE, PARENT_TYPE, STPA_NODE_TYPE } from './STPA-model';
+import { determineLayerForSTPANode } from './utils';
+
 
 export class STPALayoutConfigurator extends DefaultLayoutConfigurator {
 
@@ -8,15 +12,18 @@ export class STPALayoutConfigurator extends DefaultLayoutConfigurator {
         return {
             'org.eclipse.elk.direction': 'DOWN',
             'org.eclipse.elk.spacing.nodeNode': '30.0',
-            //'org.eclipse.elk.interactieLayout': 'true',
             'org.eclipse.elk.layered.spacing.edgeNodeBetweenLayers': '30.0'
         };
     }
 
     protected nodeOptions(snode: SNode, index: SModelIndex): LayoutOptions {
         return {
-            'org.eclipse.elk.portAlignment.default': 'CENTER',
-            'org.eclipse.elk.portConstraints': 'FIXED_SIDE'
+            'org.eclipse.elk.direction': 'DOWN',
+            'org.eclipse.elk.separateConnectedComponents': 'false',
+            'org.eclipse.elk.algorithm': 'layered',
+            'org.eclipse.elk.layered.crossingMinimization.semiInteractive': 'true',
+            'cycleBreaking.strategy': 'INTERACTIVE',
+            'layering.strategy': 'INTERACTIVE'
         };
     }
 
@@ -27,28 +34,29 @@ export class STPALayoutConfigurator extends DefaultLayoutConfigurator {
         };
     }
 
-/*     apply(element: SModelElement, index: SModelIndex): LayoutOptions | undefined {
+    apply(element: SModelElement, index: SModelIndex): LayoutOptions | undefined {
         switch (getBasicType(element)) {
             case 'graph':
                 return this.graphOptions(element as SGraph, index)
             case 'node':
-                if (element instanceof STPANode) {
-                    const layer = determineLayerForSTPANode(element)
+                if (element.type == STPA_NODE_TYPE) {
+                    const layer: number = determineLayerForSTPANode(element as STPANode);
+                    (element as SShapeElement).position = {x: 100 * layer, y: 100 * layer}
                     return {
-                        'org.eclipse.elk.port.side': 'NORTH',
+                        'org.eclipse.elk.port.side': 'DOWN',
+                        'org.eclipse.elk.port.borderOffset': '3.0'
+                    }
+                } else if (element.type == CS_NODE_TYPE) {
+                    //const layer = element.layer
+                    const layer = "0"
+                    return {
+                        'org.eclipse.elk.port.side': 'DOWN',
                         'org.eclipse.elk.port.borderOffset': '3.0',
                         'org.eclipse.elk.layered.layering.layerChoiceConstraint': layer
                     }
+                } else if (element.type == PARENT_TYPE) {
+                    return this.nodeOptions(element as SNode, index)
                 }
-                if (element instanceof CSNode) {
-                    const layer = element.layer
-                    return {
-                        'org.eclipse.elk.port.side': 'NORTH',
-                        'org.eclipse.elk.port.borderOffset': '3.0',
-                        'org.eclipse.elk.layered.layering.layerChoiceConstraint': layer
-                    }
-                }
-                return this.nodeOptions(element as SNode, index)
             case 'edge':
                 return super.edgeOptions(element as SEdge, index)
             case 'label':
@@ -58,6 +66,6 @@ export class STPALayoutConfigurator extends DefaultLayoutConfigurator {
             default:
                 return undefined
         }
-    } */
+    }
 
 }
