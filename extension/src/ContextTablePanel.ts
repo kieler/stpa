db2b881ad87c2b322bfa7cc36c3ed04652055319
Jana Kreiss <stu201977@mail.uni-kiel.de>
2022-05-23 16:01:50 +0200

    Cleaned up some code.

--- extension/src/ContextTablePanel.ts
+++ extension/src/ContextTablePanel.ts
@@ -2,29 +2,36 @@ import * as vscode from "vscode";
 import { getNonce } from "./getNonce";
 
 export class ContextTablePanel {
-  /**
-   * Track the currently panel. Only allow a single panel to exist at a time.
-   */
+  // Track the current panel. Only allow a single panel to exist. 
   public static currentPanel: ContextTablePanel | undefined;
-
   public static readonly viewType = "context-table";
 
+  // Constructor variables.
   private readonly _panel: vscode.WebviewPanel;
   private readonly _extensionUri: vscode.Uri;
   private _disposables: vscode.Disposable[] = [];
 
+  private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+    this._panel = panel;
+    this._extensionUri = extensionUri;
+
+    // Initialize the webview.
+    this._update();
+    // Listen for the panel being disposed.
+    this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+  }
+
+  // Main call method.
   public static createOrShow(extensionUri: vscode.Uri) {
     const column = vscode.window.activeTextEditor
       ? vscode.window.activeTextEditor.viewColumn
       : undefined;
-
     // If we already have a panel, show it.
     if (ContextTablePanel.currentPanel) {
       ContextTablePanel.currentPanel._panel.reveal(column);
       ContextTablePanel.currentPanel._update();
       return;
     }
-
     // Otherwise, create a new panel.
     const panel = vscode.window.createWebviewPanel(
       ContextTablePanel.viewType,
@@ -33,7 +40,6 @@ export class ContextTablePanel {
       {
         // Enable javascript in the webview
         enableScripts: true,
-
         // And restrict the webview to only loading content from our extension's `css` directory.
         localResourceRoots: [
           vscode.Uri.joinPath(extensionUri, "src"),
@@ -41,52 +47,28 @@ export class ContextTablePanel {
         ],
       }
     );
-
     ContextTablePanel.currentPanel = new ContextTablePanel(panel, extensionUri);
   }
 
+  // Kills off the current panel.
   public static kill() {
     ContextTablePanel.currentPanel?.dispose();
     ContextTablePanel.currentPanel = undefined;
   }
 
+  // Revives a defined panel.
   public static revive(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
     ContextTablePanel.currentPanel = new ContextTablePanel(panel, extensionUri);
     // Exists so the _extensionUri variable is read at least once.
     ContextTablePanel.currentPanel._extensionUri.path;
   }
 
-  private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
-    this._panel = panel;
-    this._extensionUri = extensionUri;
-
-    // Set the webview's initial html content
-    this._update();
-
-    // Listen for when the panel is disposed
-    // This happens when the user closes the panel or when the panel is closed programatically
-    this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
-
-    // // Handle messages from the webview
-    // this._panel.webview.onDidReceiveMessage(
-    //   (message) => {
-    //     switch (message.command) {
-    //       case "alert":
-    //         vscode.window.showErrorMessage(message.text);
-    //         return;
-    //     }
-    //   },
-    //   null,
-    //   this._disposables
-    // );
-  }
-
+  // Disposes of the panel and all its related data.
   public dispose() {
     ContextTablePanel.currentPanel = undefined;
 
-    // Clean up our resources
+    // Dispose of no longer needed data.
     this._panel.dispose();
-
     while (this._disposables.length) {
       const x = this._disposables.pop();
       if (x) {
@@ -95,37 +77,15 @@ export class ContextTablePanel {
     }
   }
 
+  // Update function. Used for generating and maintaining the view's content.
   private async _update() {
     const webview = this._panel.webview;
 
     this._panel.webview.html = this._getHtmlForWebview(webview);
-    webview.onDidReceiveMessage(async (data) => {
-      switch (data.type) {
-        case "onInfo": {
-          if (!data.value) {
-            return;
-          }
-          vscode.window.showInformationMessage(data.value);
-          break;
-        }
-        case "onError": {
-          if (!data.value) {
-            return;
-          }
-          vscode.window.showErrorMessage(data.value);
-          break;
-        }
-      }
-    });
   }
 
   private _getHtmlForWebview(webview: vscode.Webview) {
-    // // And the uri we use to load this script in the webview
-    /* const scriptUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "out", "compiled/table-view.js")
-    ); */
-
-    // Uri to load styles into webview
+    // Get the style sheets to be used for the HTML data
     const stylesResetUri = webview.asWebviewUri(vscode.Uri.joinPath(
       this._extensionUri,
       "src",
@@ -136,36 +96,27 @@ export class ContextTablePanel {
       "src",
       "vscode-style.css"
     ));
-    /* const cssUri = webview.asWebviewUri(vscode.Uri.joinPath(
-      this._extensionUri,
-      "out",
-      "compiled/table-view.css"
-    )); */
-
-    // Use a nonce to only allow specific scripts to be run
+    // Use a nonce for script security.
     const nonce = getNonce();
-
+    // HTML
     return `<!DOCTYPE html>
-			<html lang="en">
+		<html lang="en">
 			<head>
 				<meta charset="UTF-8">
 				<!--
-					Use a content security policy to only allow loading images from https or from our extension directory,
-					and only allow scripts that have a specific nonce.
+					Only allow scripts that have a established nonce.
         -->
-        <meta http-equiv="Content-Security-Policy" content="img-src https: data:; style-src 'unsafe-inline' ${
-      webview.cspSource
-    }; script-src 'nonce-${nonce}';">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="${stylesResetUri}" rel="stylesheet">
-		<link href="${stylesMainUri}" rel="stylesheet">
-    <script nonce="${nonce}">
-    </script>
-		</head>
-    <body>
+        <meta script-src 'nonce-${nonce}';">
+		    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		    <link href="${stylesResetUri}" rel="stylesheet">
+		    <link href="${stylesMainUri}" rel="stylesheet">
+        <script nonce="${nonce}">
+        </script>
+		  </head>
+      <body>
         <h1>Test!</h1>
         <button>Style Test!</button>
-		</body>
+		  </body>
 		</html>`;
   }
 }
