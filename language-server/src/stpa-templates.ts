d09d7438eaddd5ef2e0f544d10e15baeb91fef93
Jette Petzold <jep@mail.uni-kiel.de>
2022-06-03 16:32:59 +0200

    adjusted position calculation of CS templates

--- language-server/src/stpa-templates.ts
+++ language-server/src/stpa-templates.ts
@@ -19,6 +19,7 @@ import { Position } from 'vscode-languageserver';
 import { LanguageTemplate } from './templates/template-model';
 import { LangiumDocuments, LangiumServices } from 'langium';
 import { URI } from 'vscode-uri';
+import { TextDocument } from 'vscode-languageserver-textdocument';
 
 export class StpaTemplates {
 
@@ -49,6 +50,41 @@ export class StpaTemplates {
 
 }
 
+/**
+ * Calculates the actual text of templates for the control structure and theri position in the document.
+ * @param document The document in which the template should be inserted.
+ * @param template The template that should be inserted.
+ * @returns The position where the tempalte should be added to the {@code document}.
+ */
+function getPositionForCSTemplate(document: TextDocument, template: LanguageTemplate): Position {
+    // TODO: IDs must be unique even if the template is used more than once
+    const docText = document.getText();
+
+    // determine range of already existing definition of control structure
+    const titleIndex = docText.indexOf('ControlStructure');
+    const startIndex = titleIndex !== -1 ? titleIndex : 0;
+    const nextTitleIndex = docText.indexOf('Responsibilities');
+    const endIndex = nextTitleIndex !== -1 ? nextTitleIndex - 1 : docText.length - 1;
+    if (titleIndex === -1) {
+        template.insertText = template.baseCode;
+        return document.positionAt(endIndex);
+    } else {
+        // check whether a graph ID already exist
+        const csText = docText.substring(startIndex, endIndex);
+        const graphIndex = csText.indexOf('{');
+        // TODO: starting positions 19 and 23 may be incorrect for user provided templates
+        if (graphIndex === -1) {
+            template.insertText = template.baseCode.substring(19, template.baseCode.length);
+            return document.positionAt(endIndex);
+        } else {
+            template.insertText = template.baseCode.substring(23, template.baseCode.length-3);
+            const bracketIndex = csText.lastIndexOf('}');
+            return document.positionAt(titleIndex + bracketIndex -1);
+        }
+    }
+    
+}
+
 const simpleCSTemplateCode: string = 
 `
 ControlStructure 
@@ -68,6 +104,37 @@ CS {
 }
 `;
 
+const simpleCSWithActuatorsTemplateCode: string = 
+`
+ControlStructure 
+CS {
+    Controller {
+        hierarchyLevel 0
+        controlActions {
+            [ca "control action"] -> Actuator
+        }
+    }
+    Actuator {
+        hierarchyLevel 1
+        controlActions {
+            [caAc "control action"] -> ControlledProcess
+        }
+    }
+    Sensor {
+        hierarchyLevel 1
+        feedback {
+            [fbS "feedback"] -> Controller
+        }
+    }
+    ControlledProcess {
+        hierarchyLevel 2
+        feedback {
+            [fb "feedback"] -> Sensor
+        }
+    }
+}
+`;
+
 /**
  * Template for a control structure with one controller and one controlled process.
  */
@@ -82,28 +149,8 @@ export class SimpleCSTemplate implements LanguageTemplate {
     }
 
     getPosition (uri: string): Position {
-        // TODO: title could be written but no graph name
-        // TODO: IDs must be unique even if the template is used more than once
         const document = this.documents.getOrCreateDocument(URI.parse(uri)).textDocument;
-        const docText = document.getText();
-
-        // if there is already a control structure, the title and a graph name must be deleted
-        const titleIndex = docText.indexOf('ControlStructure');
-        if (titleIndex !== -1) {
-            this.insertText = this.baseCode.substring(23, this.baseCode.length-3);
-        } else {
-            this.insertText = this.baseCode;
-        }
-        // determine position for the text to insert
-        const nextTitleIndex = docText.indexOf('Responsibilities');
-        const endIndex = nextTitleIndex !== -1 ? nextTitleIndex - 1 : docText.length - 1;
-        if (titleIndex === -1) {
-            return document.positionAt(endIndex);
-        } else {
-            const csText = docText.substring(titleIndex, endIndex);
-            const bracketIndex = csText.lastIndexOf('}');
-            return document.positionAt(titleIndex + bracketIndex -1);
-        }
+        return getPositionForCSTemplate(document, this);
     }
 };
 
@@ -111,19 +158,17 @@ export class SimpleCSTemplate implements LanguageTemplate {
  * Template for tests
  */
 export class TestTemplate2 implements LanguageTemplate {
-    insertText = 'Losses\nL1 "test"';
-    baseCode = 'Losses\nL1 "test"';
+    insertText = simpleCSWithActuatorsTemplateCode;
+    baseCode = simpleCSWithActuatorsTemplateCode;
     documents: LangiumDocuments;
-    id: string = 'tempGraph2';
+    id: string = 'simpleCSWithAcsTemplate';
 
     constructor(documents: LangiumDocuments) {
         this.documents = documents;
     }
     
     getPosition (uri: string): Position {
-        return {
-            line: 0,
-            character: 0
-        };
+        const document = this.documents.getOrCreateDocument(URI.parse(uri)).textDocument;
+        return getPositionForCSTemplate(document, this);
     }
 };
