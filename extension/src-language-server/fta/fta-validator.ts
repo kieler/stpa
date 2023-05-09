bd5af72774e4fc62a932d2f28b6dab7bc5ec5264
Orhan <stu222573@mail.uni-kiel.de>
2023-05-09 14:44:01 +0200

    Added Conditions to Grammar and fixed behavior

--- extension/src-language-server/fta/fta-validator.ts
+++ extension/src-language-server/fta/fta-validator.ts
@@ -21,6 +21,7 @@ export class FtaValidator {
 
     checkModel(model: ModelFTA, accept: ValidationAcceptor): void{
         this.checkUniqueComponents(model, accept);
+        this.checkUniqueGates(model, accept);
     }
 
     checkUniqueComponents(model: ModelFTA, accept: ValidationAcceptor): void{
@@ -30,6 +31,15 @@ export class FtaValidator {
                 accept('error',  `Component has non-unique name '${c.name}'.`,  {node: c, property: 'name'});
             }
             componentNames.add(c.name);
+        })
+    }
+    checkUniqueGates(model:ModelFTA, accept:ValidationAcceptor): void{
+        const gateNames = new Set();
+        model.gates.forEach(g => {
+            if (gateNames.has(g.name)) {
+                accept('error',  `Gate has non-unique name '${g.name}'.`,  {node: g, property: 'name'});
+            }
+            gateNames.add(g.name);
         })
     }
 }
